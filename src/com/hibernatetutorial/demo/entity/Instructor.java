5f5722536aada5794750f3b0fd3f22504c06c44d
Divya Gupta <divygupta0319@gmail.com>
2021-05-18 21:07:46 +0530

    Tested the Eager loading of an instructor alognwith all its courses, don't need to hit db again to fetch list of courses of an instructor

--- src/com/hibernatetutorial/demo/entity/Instructor.java
+++ src/com/hibernatetutorial/demo/entity/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -51,7 +52,8 @@ public class Instructor {
 	private InstructorDetail instructorDetail;
 	
 	
-	@OneToMany(mappedBy="instructor",
+	@OneToMany(fetch=FetchType.EAGER,
+			mappedBy="instructor",
 			cascade = {
 					CascadeType.PERSIST
 					, CascadeType.DETACH
